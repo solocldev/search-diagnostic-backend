ef1af8543e9d9b6fc84c0d2cae9e54ecbc299362
solocldev <113052678+solocldev@users.noreply.github.com>
2022-09-23 15:28:15 +0530

    html

--- server.js
+++ server.js
@@ -83,14 +83,14 @@ app.post('/', async (req, res) => {
             <ul style="list-style:none;">
                 ${response.map(res => {
                 return (
-                    <div class="box" style="box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+                    `<div class="box" style="box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
                 border-radius: 12px;
                 padding: 1rem;">
                         <li class="question" style="margin:1rem 0;font-size: 18px;">Question: </li>
-                        <li class="question" style="margin:1rem 0;font-size: 24px;">{res.question} </li>
+                        <li class="question" style="margin:1rem 0;font-size: 24px;">${res.question} </li>
                         <li class="answer" style="margin:1rem 0;">Your response: <span
-                            style="font-weight: bold; font-size: 24px; color:#5D3FD3;">{res.answer}</span></li>
-                    </div>
+                            style="font-weight: bold; font-size: 24px; color:#5D3FD3;">${res.answer}</span></li>
+                    </div>`
                 )
             })}
             </ul>
