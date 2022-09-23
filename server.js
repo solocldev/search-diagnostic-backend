2f3ce1f8b30b017a9ca077ea76c8cd047af78e8d
solocldev <113052678+solocldev@users.noreply.github.com>
2022-09-23 15:22:44 +0530

    html file added

--- server.js
+++ server.js
@@ -64,34 +64,69 @@ app.post('/', async (req, res) => {
                 result++;
             }
         })
-        html_to_pdf.generatePdf({ content: "<h1>nice one</h1>" }, options).then(pdfBuffer => {
-            fs.writeFileSync('./output.pdf', pdfBuffer)
-            const msg = {
-                from: "sarthakrajesh777@gmail.com",
-                to: details.email,
-                subject: "test",
-                attachments: [{
-                    filename: 'output.pdf',
-                    path: './output.pdf',
-                    contentType: 'application/pdf'
-                }],
-                text: "test"
-            }
-            nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: 'sarthakrajesh777@gmail.com',
-                    pass: process.env.APPPASS
-                },
-                port: 465,
-                host: 'smtp.gmail.com',
-            }).sendMail(msg, (err) => {
-                if (err) {
-                    console.log(err);
+        html_to_pdf.generatePdf({
+            content: `
+        <!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <script src="server.js"></script>
+        </head>
+        
+        <body>
+            <h1 class="title" style="text-align:center; color:#5D3FD3;">Search Diagnostics</h1>
+            <h1 class="score" style="text-align:center;">{{result}}0%</h1>
+            <ul style="list-style:none;">
+                ${response.map(res => {
+                return (
+                    <>
+                        <div class="box" style="box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+                border-radius: 12px;
+                padding: 1rem;">
+                            <li class="question" style="margin:1rem 0;font-size: 18px;">Question: </li>
+                            <li class="question" style="margin:1rem 0;font-size: 24px;">{res.question} </li>
+                            <li class="answer" style="margin:1rem 0;">Your response: <span
+                                style="font-weight: bold; font-size: 24px; color:#5D3FD3;">{res.answer}</span></li>
+                        </div>
+                        <br />
+                    </>
+                )
+            })}
+            </ul>
+        </body>
+        </html>
+        ` }, options).then(pdfBuffer => {
+                fs.writeFileSync('./output.pdf', pdfBuffer)
+                const msg = {
+                    from: "sarthakrajesh777@gmail.com",
+                    to: details.email,
+                    subject: "test",
+                    attachments: [{
+                        filename: 'output.pdf',
+                        path: './output.pdf',
+                        contentType: 'application/pdf'
+                    }],
+                    text: "test"
                 }
-                else console.log('sent')
-            })
-        });
+                nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: 'sarthakrajesh777@gmail.com',
+                        pass: process.env.APPPASS
+                    },
+                    port: 465,
+                    host: 'smtp.gmail.com',
+                }).sendMail(msg, (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else console.log('sent')
+                })
+            });
 
         res.status(201).json(object)
     } catch (error) {
